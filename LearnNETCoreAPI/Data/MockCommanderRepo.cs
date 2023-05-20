0784e1da945d7ca9e91618800804996b0273c122
Aditya Prasetyo <adty404@gmail.com>
2023-05-20 07:08:51 +0700

    mock repository

--- LearnNETCoreAPI/Data/MockCommanderRepo.cs
+++ LearnNETCoreAPI/Data/MockCommanderRepo.cs
@@ -1,7 +1,30 @@
-﻿namespace Commander.Data
+﻿using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
 {
     public class MockCommanderRepo : ICommanderRepo
     {
+        // This is a mock repository, which is a fake repository that we can use to test our API.
+        // We will replace this with a real repository later
 
+        // GetAppCommands returns a list of command objects
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle & Pan" },
+                new Command { Id = 1, HowTo = "Cut bread", Line = "Get a knife", Platform = "Knife & Chopping board" },
+                new Command { Id = 2, HowTo = "Make cup of tea", Line = "Place teabag in cup", Platform = "Kettle & Cup" }
+            };
+
+            return commands;
+        }
+
+        // GetCommandById returns a single command object with the specified id
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle & Pan" };
+        }
     }
 }
