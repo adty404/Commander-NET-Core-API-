94f1a3fc115a1035f1aa3049b3da9697dbff5e96
Aditya Prasetyo <adty404@gmail.com>
2023-05-20 22:24:54 +0700

    Creating a mapping Profile

---
+++ LearnNETCoreAPI/Profiles/CommandsProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Commander.Dtos;
+using Commander.Models;
+
+namespace Commander.Profiles
+{
+    // Profile is a class that inherits from AutoMapper.Profile
+    // Profile is used to define the mapping between the source and target
+    // Source is the model class, Target is the DTO class
+    public class CommandsProfile : Profile
+    {
+        public CommandsProfile()
+        {
+            // Source -> Target
+            CreateMap<Command, CommandReadDto>();
+        }
+    }
+}
