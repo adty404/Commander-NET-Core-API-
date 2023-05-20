0ef76bc6939ddef6ef5e7ee20f17b0f7bb08ec14
Aditya Prasetyo <adty404@gmail.com>
2023-05-20 17:45:16 +0700

    modify Command Model (not null, and maxlength), and create a migration (again)

--- LearnNETCoreAPI/Models/Command.cs
+++ LearnNETCoreAPI/Models/Command.cs
@@ -1,10 +1,20 @@
-﻿namespace Commander.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Commander.Models
 {
     public class Command
     {
+        [Key] // This attribute is used to specify that a property is a primary key
         public int Id { get; set; }
+
+        [Required] // This attribute is used to specify that a property is required
+        [MaxLength(250)] // This attribute is used to specify the maximum length of a string property
         public string HowTo { get; set; }
+
+        [Required]
         public string Line { get; set; }
+
+        [Required]
         public string Platform { get; set; }
     }
 }
