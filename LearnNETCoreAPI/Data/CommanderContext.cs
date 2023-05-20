3c3928f212b8a9da05b9356c489e0bb939927d8d
Aditya Prasetyo <adty404@gmail.com>
2023-05-20 14:39:19 +0700

    db context (CommanderContext)

---
+++ LearnNETCoreAPI/Data/CommanderContext.cs
@@ -0,0 +1,17 @@
+﻿using Commander.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Commander.Data
+{
+    public class CommanderContext : DbContext
+    {
+        public CommanderContext(DbContextOptions<CommanderContext> opt) : base(opt)
+        {
+            
+        }
+
+        // DbSet represents a collection of entities from the database
+        // DbSet is a property of DbContext, Command is the model class, get and set are accessors
+        public DbSet<Command> Commands { get; set; }
+    }
+}
