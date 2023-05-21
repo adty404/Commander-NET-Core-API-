be04fe3eeddc70ca93ff1aed8d7532b7534d7405
Aditya Prasetyo <adty404@gmail.com>
2023-05-21 11:14:03 +0700

    Add Annotations to DTO to avoid 500 errors

--- LearnNETCoreAPI/Dtos/CommandCreateDto.cs
+++ LearnNETCoreAPI/Dtos/CommandCreateDto.cs
@@ -1,11 +1,17 @@
-﻿namespace Commander.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Commander.Dtos
 {
     public class CommandCreateDto
     {
+        [Required] // This attribute is used to specify that a property is required
+        [MaxLength(250)] // This attribute is used to specify the maximum length of a string property
         public string HowTo { get; set; }
 
+        [Required]
         public string Line { get; set; }
 
+        [Required]
         public string Platform { get; set; }
     }
 }
