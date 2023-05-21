6e8472198d86201ba76f6d96a566ae26bd2d6aa4
Aditya Prasetyo <adty404@gmail.com>
2023-05-21 11:25:01 +0700

    Add a new DTO (for Updating)

---
+++ LearnNETCoreAPI/Dtos/CommandUpdateDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Commander.Dtos
+{
+    public class CommandUpdateDto
+    {
+        [Required] // This attribute is used to specify that a property is required
+        [MaxLength(250)] // This attribute is used to specify the maximum length of a string property
+        public string HowTo { get; set; }
+
+        [Required]
+        public string Line { get; set; }
+
+        [Required]
+        public string Platform { get; set; }
+    }
+}
